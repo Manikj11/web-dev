d14e01d1a3015fb683b5ce5da563565115012668
manik jain <jainmanik1199@gmail.com>
2022-03-19 13:37:23 +0530

    objects in JS

---
+++ Module 2/objects.js
@@ -0,0 +1,23 @@
+//empty object, have key value pair like dictionary
+let obj = {};
+console.log(obj)
+let ob={
+    "name": 'manik', "age":98,number:9887766
+}
+console.log(ob)
+
+let avengers={
+    name:'steve',
+    friends:['natasha','thor','bucky'],
+    baap: 'iron man',
+    age:99,
+    address : {
+        city: 'haryana',
+        state:'queens'
+    },
+    sayhi:function(){
+        console.log('this function inside object is running');
+    }
+
+}
+console.log(avengers)
