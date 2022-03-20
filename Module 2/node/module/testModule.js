e79fca4eda19becb4733b4e17d0ee42e9c16be42
manik jain <jainmanik1199@gmail.com>
2022-03-20 15:03:17 +0530

    module

---
+++ Module 2/node/module/testModule.js
@@ -0,0 +1,11 @@
+let cal = require("./calculator.js")
+console.log(cal)
+
+a = cal.addition(6,7);
+b= cal.subtraction(98,43);
+c= cal.divide(50,2);
+d = cal.multiply(2,3)
+ console.log("add = "+a)
+ console.log("sub = "+b)
+ console.log("divide = "+c)
+ console.log("multiply = "+d)
