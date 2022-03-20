02ab723852e2af949f5ce8f237318dc287034116
manik jain <jainmanik1199@gmail.com>
2022-03-20 14:09:49 +0530

    CRUD for file complete

--- Module 2/node/fs.js
+++ Module 2/node/fs.js
@@ -6,10 +6,17 @@ let path = require("path")
 let filePath = path.join(__dirname,"text.txt");
 console.log(filePath);
 
-//create new  file in that filepath if not exist 
+//create new  file in that filepath if not exist --Creating C
 fs.writeFileSync(filePath,"Hello I am a text file created and this is the content that goes inside")
 fs.writeFileSync(filePath,"writing again") //overrides ths previous content
+//reading --R
+let content = fs.readFileSync(filePath,'utf-8');
+console.log(content)
 
+//updating file...where content is not overridden--U
 
+fs.appendFileSync(filePath,"newly added content which is not overridden")
+console.log("after update: "+ fs.readFileSync(filePath,'utf-8') )
 
-
+//delete --D
+fs.unlinkSync(filePath);
